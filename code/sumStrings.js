d1e2f052e56916864af93ad21ef1cfea1ed1f5b9
Jelon <jangdelong@qq.com>
2022-01-05 17:21:09 +0800

    Create sumStrings.js

---
+++ code/sumStrings.js
@@ -0,0 +1,12 @@
+function sumStrings (a, b) {
+    var res = '',
+        c = 0;
+    a = a.split('');
+    b = b.split('');
+    while (a.length || b.length || c) {
+        c += ~~a.pop() + ~~b.pop(); // ~~a字符串转数字 
+        res = c % 10 + res;
+        c = c > 9; // 进1
+    }
+    return res.replace(/^0+/,'');
+}
